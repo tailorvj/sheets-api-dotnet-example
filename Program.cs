effa383b30a43251a212195f577ed3d8800e5a36
Tailor VJ <tailor.vj@gmail.com>
2023-07-31 10:38:03 +0000

    read and write to a Google Sheets document via Sheets API

--- Program.cs
+++ Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace dotnetcore
+class Program
 {
-    class Program
+    static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+    static readonly string SpreadsheetId = Environment.GetEnvironmentVariable("SPREADSHEET_ID");
+    //make sure you create a service account in Google Cloud Console and save the key to your project root
+    static readonly string ServiceAccountPath = "credentials.json"; // replace with your service account file path
+    static SheetsService service;
+
+    //replace with your sheet name
+    static readonly string SheetName = "Sheet1";
+    //change data inside the object to your own strings
+    static readonly List<object> NewData = new List<object> { "Another", "Update", "Go!" };
+
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        GoogleCredential credential;
+        using (var stream = new FileStream(ServiceAccountPath, FileMode.Open, FileAccess.Read))
         {
-            Console.WriteLine("Hello World!");
+            credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
         }
+
+        // Create Google Sheets API service.
+        service = new SheetsService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = "Google Sheets API .NET Quickstart",
+        });
+
+        // Read values from the Sheet.
+        var readRange = $"{SheetName}"; // replace with your Sheet Name and Range
+        var response = await service.Spreadsheets.Values.Get(SpreadsheetId, readRange).ExecuteAsync();
+        Console.WriteLine("Reading values from the sheet:");
+        foreach (var row in response.Values)
+        {
+            Console.WriteLine(string.Join(", ", row));
+        }
+
+        // Read values from the Sheet to determine the next available row.
+        var readUpdateRange = $"{SheetName}!A:A"; // Read entire column A of the specified sheet
+        var readResponse = await service.Spreadsheets.Values.Get(SpreadsheetId, readUpdateRange).ExecuteAsync();
+        int nextRow = (readResponse.Values?.Count ?? 0) + 1; // Determine next available row
+
+        // Write values to the Sheet starting at the next available row in column A.
+        var writeRange = $"{SheetName}!A{nextRow}"; // Set the write range to start at the next available row
+        var valueRange = new ValueRange() { Values = new List<IList<object>> { NewData } };
+        var updateRequest = service.Spreadsheets.Values.Update(valueRange, SpreadsheetId, writeRange);
+        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+        var update = await updateRequest.ExecuteAsync();
+
+        Console.WriteLine($"Updated {update.UpdatedCells} cells.");
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# A .NET Core CLI template on Gitpod
+# Read and write from/to a Google Sheets spreadsheet
 
+* Fork this project
+* Sign up for gitpod and open this project in a Gitpod workspace
+* Login to Google Cloud Console
+* Create a new project or use an existing one
+* Add the Google Sheets API to your project
+* Create a service account and make it an owner for Google Sheets
+* Create and download credentials.json
+* copy credentials.json to the project root
+* Create a spreadsheet - https://sheets.new or use an existing one
+* Manually write some data to the spreadsheet for reading function
+* copy the spreadsheet id from the sheets doc URL
+* In the gitpod environment terminal: export SPREADSHEET_ID="yourspreadsheetid"
+* Variables to manually edit in Program.cs as needed: SheetName, NewData
+* dotnet run - will log existing data to the console and append the new data
+* Check the spreadsheet for new cells written from this script
+* Modify the script as you wish
+
+## About The Gitpod Development environment
 This is a [.NET Core CLI](https://docs.microsoft.com/en-us/dotnet/core/introduction) template configured for ephemeral cloud development environments on [Gitpod](https://www.gitpod.io/).
 
 ## Next Steps
